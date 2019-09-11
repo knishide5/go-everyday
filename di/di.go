be826593d1ea9486a060fd10da7d458c2316a1cf
Koichi <knishide5@gmail.com>
2019-09-12 01:46:53 +0900

    change http

--- di/di.go
+++ di/di.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+  "net/http"
 )
 
 func Greet(writer io.Writer, name string) {
   fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+  Greet(w, "world")
+}
+
 func main() {
-  Greet(os.Stdout, "Elodie")
+  err := http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+
+  if err != nil {
+    fmt.Println(err)
+  }
 }
