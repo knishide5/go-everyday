165b5cfab5f4c203118e61395bb79db489cce57e
Koichi <knishide5@gmail.com>
2019-09-23 17:54:30 +0900

    implement node

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -1,19 +1,23 @@
 package ast
 
+import "go-everyday/monkey/token"
+
+// ASTのすべてのノードはNodeインターフェースを実装する必要あり
 type Node interface {
 	TokenLiteral() string
 }
 
 type Statement interface {
 	Node
-	statmentNode()
+	statmentNode() // dummy
 }
 
 type Expression interface {
 	Node
-	expressionNode()
+	expressionNode() // dummy
 }
 
+// ASTのすべてのノードのルート
 type Program struct {
 	Statements []Statement
 }
@@ -24,4 +28,25 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
