51c7acaeb67bd61ef0b1a535fb6f7344f8b3e753
Koichi <knishide5@gmail.com>
2019-10-06 12:53:53 +0900

    implement ParseProgram

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -1,15 +1,15 @@
 package parser
 
 import (
-  "go-everyday/monkey/token"
-  "go-everyday/monkey/lexer"
-  "go-everyday/monkey/ast"
+	"go-everyday/monkey/ast"
+	"go-everyday/monkey/lexer"
+	"go-everyday/monkey/token"
 )
 
 type Parser struct {
 	l *lexer.Lexer // 字句解析器へのポインタ
 
-	curToken token.Token // 現在のToken
+	curToken  token.Token // 現在のToken
 	peekToken token.Token // 次のToken
 }
 
@@ -27,5 +27,15 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
 }
